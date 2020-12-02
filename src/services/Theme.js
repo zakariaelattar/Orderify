48f4ac07d9cd5f8d2834f1d181538297e06eb55e
zakaria <elattarz@yahoo.com>
2020-12-02 00:21:39 +0000

    clean code theme

--- src/services/Theme.js
+++ src/services/Theme.js
@@ -1,13 +1,16 @@
 const axios = require("axios");
 const jsdom = require("jsdom");
 const fs = require("fs");
+const { getArgumentValues } = require("graphql/execution/values");
 const { JSDOM } = jsdom;
-const shopRequestUrl = 'https://mystoreofdev.myshopify.com/admin/api/2020-10/assets.json?asset[key]=sections/product-template.liquid';
+const GET_ASSET_ENDPOINT = 'https://mystoreofdev.myshopify.com/admin/api/2020-10/assets.json?asset[key]=sections/product-template.liquid';
+const PUT_ASSET_ENDPOINT = 'https://mystoreofdev.myshopify.com/admin/api/2020-10/assets.json';
+
 var shopRequestHeaders;
 // product.liquid asset
 //https://mystoreofdev.myshopify.com/admin/api/2020-10/assets.json?asset[key]=templates/product.liquid
 //https://mystoreofdev.myshopify.com/admin/api/2020-10/assets.json?asset[key]=sections/product-template.liquid
-var modalCode;
+var MODAL_CODE;
 
 // get modal code from database
 // get web product page from shopify
@@ -20,76 +23,67 @@ var modalCode;
 
 const getModalHtml = () => {
   
-  fs.readFile("public/modal.html","utf-8",(err,data)=>{modalCode = data});
+  fs.readFile("public/modal.html","utf-8",(err,data)=>{ MODAL_CODE =  data});
 
-  try {
-    const dom = new JSDOM(shopResponse.data.asset.value);
-    let wrapper = dom.window.document.getElementsByClassName("product-form__item product-form__item--submit{% if section.settings.enable_payment_button %} product-form__item--payment-button{% endif %}{% if product.has_only_default_variant %} product-form__item--no-variants{% endif %}")[0];
-    fs.readFile("public/modal.html","utf-8",(err,data)=>{
-      modalCode = data;
-      console.log(modalCode);
-      dom.window.document.getElementsByClassName("product-form__item product-form__item--submit{% if section.settings.enable_payment_button %} product-form__item--payment-button{% endif %}{% if product.has_only_default_variant %} product-form__item--no-variants{% endif %}")[0].innerHTML = modalCode;
-      let same = dom.window.document.getElementsByTagName("BODY")[0].innerHTML;
-        fs.writeFile("block.liquid",same,() =>{
-        this.addModal(same,access_token)
-        console.log("yes");
-});
-    });
-    //console.log(same);
+//   try {
+//     const dom = new JSDOM(shopResponse.data.asset.value);
+//     let wrapper = dom.window.document.getElementsByClassName("product-form__item product-form__item--submit{% if section.settings.enable_payment_button %} product-form__item--payment-button{% endif %}{% if product.has_only_default_variant %} product-form__item--no-variants{% endif %}")[0];
+//     fs.readFile("public/modal.html","utf-8",(err,data)=>{
+//       modalCode = data;
+//       console.log(modalCode);
+//       dom.window.document.getElementsByClassName("product-form__item product-form__item--submit{% if section.settings.enable_payment_button %} product-form__item--payment-button{% endif %}{% if product.has_only_default_variant %} product-form__item--no-variants{% endif %}")[0].innerHTML = modalCode;
+//       let same = dom.window.document.getElementsByTagName("BODY")[0].innerHTML;
+//         fs.writeFile("block.liquid",same,() =>{
+//         this.addModal(same,access_token)
+//         console.log("yes");
+// });
+//     });
+//     //console.log(same);
 
    
-  }
-  catch(e) {
-      console.log(e); 
-           }
+//   }
+//   catch(e) {
+//       console.log(e); 
+//            }
 }
 
 /** 
 get product page from shopify
 */
 const getProductPage = async (url,header) => {
-  return axios.get(url,{ headers: header }); x;
-//   axios.get(url,
-//   { headers: header })
-// .then((shopResponse) => {
-//  (shopResponse.data.asset.value);
-//       try {
-//         const dom = new JSDOM(shopResponse.data.asset.value);
-//         let wrapper = dom.window.document.getElementsByClassName("product-form__item product-form__item--submit{% if section.settings.enable_payment_button %} product-form__item--payment-button{% endif %}{% if product.has_only_default_variant %} product-form__item--no-variants{% endif %}")[0];
-//         fs.readFile("public/modal.html","utf-8",(err,data)=>{
-//           modalCode = data;
-//           console.log(modalCode);
-//           dom.window.document.getElementsByClassName("product-form__item product-form__item--submit{% if section.settings.enable_payment_button %} product-form__item--payment-button{% endif %}{% if product.has_only_default_variant %} product-form__item--no-variants{% endif %}")[0].innerHTML = modalCode;
-//           let same = dom.window.document.getElementsByTagName("BODY")[0].innerHTML;
-//             fs.writeFile("block.liquid",same,() =>{
-//             this.addModal(same,access_token)
-//             console.log("yes");
-//  });
-//         });
-//         //console.log(same);
+  return axios.get(url,{ headers: header });
 
-
-//       }
-//       catch(e) {
-//           console.log(e); 
-//                }
-
-// })
-// .catch((error) => {
-// console.log(error)
-// });
- 
+}
+const putProductPage = async (url,header,page_code) => {
+  return 
+  axios.put(url,
+   {  
+       "asset": {
+         "key": "sections/product-template.liquid",
+         "value":page_code
+       }
+     },
+    { headers: header }
+);
 
 }
 exports.processProductPage = async (req,res) => {
-    let access_token = req.headers["x-shopify-access-token"];
-     shopRequestHeaders = {'X-Shopify-Access-Token': access_token};
-     getProductPage(shopRequestUrl,shopRequestHeaders).then((productPage) => {
-     let dom =  new JSDOM(productPage);
-     console.log(productPage);
-      console.log(dom.window.document.getElementsByClassName("product-form__item product-form__item--submit{% if section.settings.enable_payment_button %} product-form__item--payment-button{% endif %}{% if product.has_only_default_variant %} product-form__item--no-variants{% endif %}")[0]);
+        let access_token = req.headers["x-shopify-access-token"];
+        shopRequestHeaders = {'X-Shopify-Access-Token': access_token};
+        getModalHtml();
+        getProductPage(GET_ASSET_ENDPOINT,shopRequestHeaders)
+                .then((shopify_response) => {
+              string_product_page = shopify_response.data.asset.value
+              let dom_product_page =   bindResponseToDom(string_product_page);
+              let block = extractAddCartCode(dom_product_page);
+              block.innerHTML = MODAL_CODE;
 
-     })
+              let page_body = dom_product_page.window.document.getElementsByTagName("BODY")[0].innerHTML;
+              let final_code = correctingDocument(page_body);
+
+              save(final_code,access_token);
+             
+    })
      .catch((err) => {
        console.log(err);
      })
@@ -108,19 +102,39 @@ const bindResponseToDom = (liquid_data) => {
 extracting add to cart and buy now code to replace it by the built code
 */
 const extractAddCartCode = (dom) => {
-  console.log("extracting");
-  return dom
-  .window
-  .document
-  .getElementsByClassName("product-form__item product-form__item--submit{% if section.settings.enable_payment_button %} product-form__item--payment-button{% endif %}{% if product.has_only_default_variant %} product-form__item--no-variants{% endif %}")[0];
+  return extractElementFromDocument(dom,"product-form__item product-form__item--submit{% if section.settings.enable_payment_button %} product-form__item--payment-button{% endif %}{% if product.has_only_default_variant %} product-form__item--no-variants{% endif %}");
+  
+}
+/** 
+extracting by className from document
+*/
+const extractElementFromDocument = (dom, className) => {
+  return dom.window.document.getElementsByClassName(className)[0];
+
+}
+/**  
+replace the code with the built code
+*/
+const replaceCode = (modal_code, original_code) => {
+  dom.window.document.getElementsByClassName("product-form__item product-form__item--submit{% if section.settings.enable_payment_button %} product-form__item--payment-button{% endif %}{% if product.has_only_default_variant %} product-form__item--no-variants{% endif %}")[0].innerHTML = modalCode;
 
 }
 /** 
-replace the code with the built code
+correcting document
 */
-const replaceCode = (modal_code,original_code) => {
-  dom.window.document.getElementsByClassName("product-form__item product-form__item--submit{% if section.settings.enable_payment_button %} product-form__item--payment-button{% endif %}{% if product.has_only_default_variant %} product-form__item--no-variants{% endif %}")[0].innerHTML = modalCode;
-
+const correctingDocument = (html) => {
+return html
+.replace(/" endif  /g, '" {% endif %}  ')
+ .replace(/" endif/g, '" {% endif %} ')
+ .replace(/"="/g, '=')
+ .replace(/option.selected_value== %}/g, 'option.selected_value==value %}')
+ .replace(/{%="" if="" option.selected_value="=value" %}/g, '{%  if  option.selected_value==value %}')
+ .replace(/= %}/g, ' ')
+ .replace(/=""/g, ' ')
+ .replace(/&gt;/g,'>')
+ .replace(/&lt;/g,'<')
+ .replace(/" endif  %} /g,'"{% endif %}')
+ .replace(/" endunless/g,'" {%endunless');
 }
 
 /** 
@@ -132,51 +146,22 @@ const createBackup = () => {
 /** 
 save the product page and send it to shopify
 */
-const save = (htmlCode,a) => {
-
-
-  const new_code = htmlCode
-  .replace(/" endif  /g, '" {% endif %}  ')
-   .replace(/" endif/g, '" {% endif %} ')
-   .replace(/"="/g, '=')
-   .replace(/option.selected_value== %}/g, 'option.selected_value==value %}')
-   .replace(/{%="" if="" option.selected_value="=value" %}/g, '{%  if  option.selected_value==value %}')
-   .replace(/= %}/g, ' ')
-   .replace(/=""/g, ' ')
-   .replace(/&gt;/g,'>')
-   .replace(/&lt;/g,'<')
-   .replace(/" endif  %} /g,'"{% endif %}')
-   .replace(/" endunless/g,'" {%endunless');
-    let access_token = a;
-          
-            fs.writeFile("block.liquid",new_code,() =>{
-              console.log("yes");
-            });
-      console.log(htmlCode);
-
-
-    const shopRequestUrl = 'https://mystoreofdev.myshopify.com/admin/api/2020-10/assets.json';
-     const shopRequestHeaders = {
-      'X-Shopify-Access-Token': access_token
-    };
-
-
-   axios.put(shopRequestUrl,
-    {  
-        "asset": {
-          "key": "sections/product-template.liquid",
-          "value":new_code
-        }
-      },
-     { headers: shopRequestHeaders }
-)
-    .then((shopResponse) => {
-      
-     console.log(shopResponse);
-    })
-    .catch((error) => {
-        console.log(error.config.data);
-    });
+const save = async (htmlCode,accessToken) => {
+  let access_token = accessToken;
+  putProductPage(PUT_ASSET_ENDPOINT,
+     { headers: shopRequestHeaders },
+      htmlCode) .then((response) => {
+        console.log("content added successfully !!")    
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log("error occured")    
+          console.log(err);
+      })
+  // fs.writeFile("block.liquid",htmlCode,
+  // () =>{
+  //       console.log("yes");
+  //     });
 }
 
 
